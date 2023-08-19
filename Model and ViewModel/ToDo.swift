f9f4b38604ea88f91d73193579f3a6314548f44c
Ethan C <ethan.chow.yi.xiang@sji.edu.sg>
2023-08-19 10:03:01 +0800

    Some Updates

---
+++ Model and ViewModel/ToDo.swift
@@ -0,0 +1,16 @@
+//
+//  ToDo.swift
+//  Todo Demo App
+//
+//  Created by Ethan Chow on 19/8/23.
+//
+
+import Foundation
+
+struct Todo: Identifiable {
+    
+    var id = UUID()
+    var title: String
+    var subtitle = ""
+    var isDone = false
+}
