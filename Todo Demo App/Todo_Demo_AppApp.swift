84ba1b971b795d015dd04107c4858c487d105382
Ethan C <ethan.chow.yi.xiang@sji.edu.sg>
2023-08-19 09:55:59 +0800

    Initial Commit

---
+++ Todo Demo App/Todo_Demo_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Todo_Demo_AppApp.swift
+//  Todo Demo App
+//
+//  Created by Ethan Chow on 19/8/23.
+//
+
+import SwiftUI
+
+@main
+struct Todo_Demo_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
